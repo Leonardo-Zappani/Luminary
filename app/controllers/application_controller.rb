c1cd5b0c39a87bebc221f8d06ea4d68ffa1d2f72
Leonardo-Zappani <leonardozappani@alunos.utfpr.edu.br>
2024-03-12 21:24:22 -0300

    creating default page for author

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # uncomment to allow extra User model params during registration (beyond email/password)
   # before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_in_path_for(resource)
-    current_user.admin? ? dashboard_index_path : blog_posts_index_path
+    current_user.admin? ? dashboard_index_path : blog_posts_path
   end
 
   def maybe_skip_onboarding
