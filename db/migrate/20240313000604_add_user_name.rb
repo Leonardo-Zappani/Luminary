ac6d4d7aac49837f2c2e50da5940f99d936c7e11
Leonardo-Zappani <leonardozappani@alunos.utfpr.edu.br>
2024-03-18 21:11:46 -0300

    fixing migration

--- db/migrate/20240313000604_add_user_name.rb
+++
@@ -1,5 +0,0 @@
-class AddUserName < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :name, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_13_000604) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_12_232925) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -79,6 +79,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_13_000604) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -89,7 +90,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_13_000604) do
     t.boolean "paying_customer", default: false
     t.string "stripe_subscription_id"
     t.string "permissions", default: [], array: true
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
